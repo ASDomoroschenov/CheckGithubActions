00ddba2329cf01e37f197b354f4845174f38fde6
ASDomoroschenov <sashachicha228@mail.ru>
2024-09-29 20:30:42 +0300

    check

---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+
+  public void test() {
+    System.out.println("Проверка что");System.out.println("Чекстайл не распространяется на тестовые классы ааааааааааааааааааааааа");
+  }
+}
