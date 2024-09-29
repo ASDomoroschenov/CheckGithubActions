bf749140b2cd7dafae9f601a1ff7503f309c19c5
ASDomoroschenov <sashachicha228@mail.ru>
2024-09-29 20:29:04 +0300

    check

--- src/main/java/ru/mai/Main.java
+++ src/main/java/ru/mai/Main.java
@@ -2,6 +2,7 @@ package ru.mai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");System.out.println("TEST");
+        System.out.println("Hello world!");
+        System.out.println("TEST");
     }
 }
